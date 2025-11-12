f95f98e462f6d688f6a04681a4e7c178427468ea
Augustine Mayaki <2509984@leedstrinity.ac.uk>
2025-11-12 08:59:39 +0000

    Implement authetication pages

--- app.py
+++ app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def login():
     return render_template('pages/auth/login.html')
 
+@app.route("/forgot-password")
+def forgot_password():
+    return render_template('pages/auth/forgot_password.html')
+
+@app.route("/verify-mfa", methods=['GET', 'POST'])
+def verify_mfa():
+    return render_template('pages/auth/verify.html')
+
+@app.route("/set_password")
+def set_password():
+    return render_template('pages/auth/set_password.html')
+
+
 if __name__=='__main__':
     app.run(debug=True)
